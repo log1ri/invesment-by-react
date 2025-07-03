b67bf2aa9eb9369a7401bc16a7d7e692daac5837
log1ri <feldekper@gmail.com>
2025-07-03 17:39:51 +0700

    immplemnt header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,9 @@
+import logo from '../assets/investment-calculator-logo.png'
 export default function Header() {
   return (
-    <header>
-      <h1>My App</h1>
+    <header id ="header">
+        <img src={logo} alt="Logo showing a money bag" />
+        <h1>Investment Calculator</h1>
     </header>
   );
 }
